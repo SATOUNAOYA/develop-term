eb7a53d9417b4768773b25934f868eb1f77aaa1c
”sato” <naoya120328@icloud.com>
2020-04-12 05:35:41 +0000

    laravel

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,8 +2,8 @@
 
 <div class="login">
 	<h1>Login</h1>
-    <form method="post">
-    	<input type="text" name="u" placeholder="メールアドレス" required="required" />
+    <form action="{{ action('ContactController@reservationform') }}"　method="post">
+    	<input type="text" name="u" placeholder="名前" required="required" />
         <input type="password" name="p" placeholder="パスワード" required="required" />
         <button type="submit" class="btn btn-primary btn-block btn-large">ログイン</button>
     </form>
